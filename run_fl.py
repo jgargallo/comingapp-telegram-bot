274f732bc548f90ed791b1e1e0c7dbed11857db5
Jose Gargallo <jgargallo@gmail.com>
2015-08-02 21:23:46 +0200

    flask version

---
+++ run_fl.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+import settings
+from utils import db
+import telegram
+from coming_bot import build_bot
+
+app = Flask(__name__)
+bot = None
+
+@app.route("/comingbot/{0}".format(settings.TG_API_KEY), methods=['GET', 'POST'])
+def post():
+    print request.get_data()
+    return "Hello World!"
+
+if __name__ == "__main__":
+    session = db.load_session(settings.MYSQL_ENGINE)
+    bot = build_bot(session, settings.TG_API_KEY)
+    app.run()
