ea13560c980d5ac6122529c42e1ea0d12f9de375
jgargallo <jgargallo@gmail.com>
2015-08-27 09:00:11 +0200

    case insensitive commands

--- run_fl.py
+++ run_fl.py
@@ -13,15 +13,16 @@ bot = None
 def post():
     body = request.json
     message = telegram.Update.de_json(body).message
-    if message.text.startswith('/new'):
+    cmd = message.text.lower()
+    if cmd.startswith('/new'):
         bot.new_cmd(message)
-    elif message.text.startswith('/yes'):
+    elif cmd.startswith('/yes'):
         bot.yes_cmd(message)
-    elif message.text.startswith('/no'):
+    elif cmd.startswith('/no'):
         bot.no_cmd(message)
-    elif message.text.startswith('/who'):
+    elif cmd.startswith('/who'):
         bot.who_cmd(message)
-    elif message.text.startswith('/maybe'):
+    elif cmd.startswith('/maybe'):
         bot.maybe_cmd(message)
     return json.dumps(body)
 
